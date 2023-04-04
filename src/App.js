70484dca251ad49b1b081fc8db5a2722d0de2d58
mahru <mahrusferdous@gmail.com>
2023-04-04 06:03:45 -0400

    Base commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={`${process.env.PUBLIC_URL}/islam.png`} className="App-logo" alt="Islam" />
+                {/* <img src={"./images/islam.jpg"} className="App-logo" alt="islam" /> */}
+                <p>Are you living in a Godless society?</p>
+                <a className="App-link" href="https://quran.com/" target="_blank" rel="noopener noreferrer">
+                    Learn Islam
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
