e4ebd8099448eb6a11cfcbf9797443b29c4144ff
mahru <mahrusferdous@gmail.com>
2023-06-12 21:19:22 -0400

    typescript fix

--- src/components/Roadmaps/DiagramNode.tsx
+++ src/components/Roadmaps/DiagramNode.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import styles from "../../styles/Diagram.module.css";
 import Link from "next/link";
 
-const DiagramNode = () => {
+const DiagramNode: React.FC = () => {
     return (
         <div className={styles.container}>
             <div className={styles.main}>
                 <div className={styles.grid}>
                     <Link href="/diagram" className={styles.card}>
                         <h2>Pillars</h2>
-                        <p>The Five Pillars are the foundation of a Muslim's faith and practice. </p>
+                        <p>The Five Pillars are the foundation of a Muslim&apos;s faith and practice. </p>
                     </Link>
                     <Link href="/prophets" className={styles.card}>
                         <h2>Prophets</h2>
