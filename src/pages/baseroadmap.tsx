644430d70bec6f3bc5fc9827d9d14ecf0454755e
mahru <mahrusferdous@gmail.com>
2023-07-05 21:41:40 -0400

    update data

--- src/pages/baseroadmap.tsx
+++ src/pages/baseroadmap.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import Navbar from "../components/Head/Navbar";
 import Footer from "../components/Footer/Footer";
 import styles from "../styles/Home.module.css";
-import BaseComponent from "../components/Roadmaps/BaseRoadmap/BaseComponent";
+import Base from "../components/Roadmaps/BaseRoadmap/Base";
 
 const base = () => {
     return (
         <div className={styles.container}>
             <Navbar />
-            <BaseComponent />
+            <Base />
             <Footer />
         </div>
     );
