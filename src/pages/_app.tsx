11b284a74fd3e8ed424dcc6113d89e251eea3221
mahru <mahrusferdous@gmail.com>
2023-07-17 02:54:23 -0400

    commits

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/light/globals.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "../components/Context/ThemeContext";
 
